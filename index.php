9f5e49e8715c1c04b43ce8c527217327ce2fab19
rahmidelshop <rahmi.delvita22@gmail.com>
2018-02-21 23:14:13 +0700

    initial

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Rahmidel Shop</title>
+<link href="styles/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="styles/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Page level plugin CSS-->
+  <link href="styles/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="styles/css/sb-admin.css" rel="stylesheet">
+</head>
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <a class="navbar-brand" href="index.php">Rahmidel Shop</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="index.php">
+            <i class="fa fa-fw fa-dashboard"></i>
+            <span class="nav-link-text">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+          <a class="nav-link" href="charts.html">
+            <i class="fa fa-fw fa-area-chart"></i>
+            <span class="nav-link-text">Charts</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Penjualan">
+          <a class="nav-link" href="penjualan.php">
+            <i class="fa fa-fw fa-shopping-basket"></i>
+            <span class="nav-link-text">Penjualan</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Pembelian">
+          <a class="nav-link" href="pembelian.php">
+            <i class="fa fa-fw fa-shopping-cart"></i>
+            <span class="nav-link-text">Pembelian</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Biaya">
+          <a class="nav-link" href="biaya.php">
+            <i class="fa fa-fw fa-money"></i>
+            <span class="nav-link-text">Biaya</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Pelanggan">
+          <a class="nav-link" href="pelanggan.php">
+            <i class="fa fa-fw fa-users"></i>
+            <span class="nav-link-text">Pelanggan</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Supplier">
+          <a class="nav-link" href="supplier.php">
+            <i class="fa fa-fw fa-chain"></i>
+            <span class="nav-link-text">Supplier</span>
+          </a>
+        </li>
+         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Produk">
+          <a class="nav-link" href="produk.php">
+            <i class="fa fa-fw fa-product-hunt"></i>
+            <span class="nav-link-text">Produk</span>
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-fw fa-envelope"></i>
+            <span class="d-lg-none">Messages
+              <span class="badge badge-pill badge-primary">12 New</span>
+            </span>
+            <span class="indicator text-primary d-none d-lg-block">
+              <i class="fa fa-fw fa-circle"></i>
+            </span>
+          </a>
+          
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-fw fa-bell"></i>
+            <span class="d-lg-none">Alerts
+              <span class="badge badge-pill badge-warning">6 New</span>
+            </span>
+            <span class="indicator text-warning d-none d-lg-block">
+              <i class="fa fa-fw fa-circle"></i>
+            </span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <form class="form-inline my-2 my-lg-0 mr-lg-2">
+            <div class="input-group">
+              <input class="form-control" type="text" placeholder="Search for...">
+              <span class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  <i class="fa fa-search"></i>
+                </button>
+              </span>
+            </div>
+          </form>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+            <i class="fa fa-fw fa-sign-out"></i>Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+    <!-- /.container-fluid-->
+    <!-- /.content-wrapper-->
+    <footer class="sticky-footer">
+      <div class="container">
+        <div class="text-center">
+          <small>Copyright © Your Website 2018</small>
+        </div>
+      </div>
+    </footer>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fa fa-angle-up"></i>
+    </a>
+    <!-- Logout Modal-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="login.html">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+    <!-- Custom scripts for this page-->
+    <script src="js/sb-admin-datatables.min.js"></script>
+    <script src="js/sb-admin-charts.min.js"></script>
+  </div>
+  </body>
+</html>
---
+++ sources/pembelian.php
@@ -0,0 +1,129 @@
+<?php include 'index.php'; ?>
+
+	<div class="content-wrapper">
+		<div class="container-fluid">
+			<div class="row">
+
+				<div class="col-lg-12">
+					<div class="table-container table-navigation">
+						<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12"
+							style="display: flex;">
+							<h2 id="invoice-title" style="display: inline-block">Transaksi
+								Pembelian</h2>
+							<h2 id="partial-title" style="padding-left: 0.5em;"></h2>
+						</div>
+						
+					<div class="card-body">
+							<div class="table-responsive">
+							<table class="table table-hover">
+								<thead>
+									<tr>
+										<th class="text-center"><input type="checkbox"
+											name="check_all_invoice" id="check_all_invoice" value="1"
+											class="disabled hidden"></th>
+										<th><a class="sort_header" data-value="nomor_transaksi"
+											data-order="" href="#">Nomor</a></th>											
+										<th><a class="sort_header" data-value="transaction_date"
+											data-order="" href="#">Tanggal</a></th>
+										<th class="sort_header"><a class="sort_header"
+											data-value="kode_produk" data-order="" href="#">Kode Produk</a>
+										</th>
+										<th><a class="sort_header" data-value="harga"
+											data-order="" href="#">Harga Beli</a></th>
+											<th><a class="sort_header" data-value="supplier"
+											data-order="" href="#">Supplier</a></th>
+										<th><a class="sort_header" data-value="qty" data-order=""
+											href="#">Qty</a></th>
+										<th><a class="sort_header"
+											data-value="total" data-order="" href="#">Total</a>
+										</th>
+										
+									</tr>
+								</thead>
+				</div>
+							</table>
+											<div class="col-sm-6 col-md-6">
+					<a class="btn btn-primary" href="/invoices/new"> <i
+						class="fa fa-plus"></i> Buat Penjualan Baru
+					</a>
+				</div>
+						</div>
+					</div>
+
+					</div>
+
+					<nav class="paging-nav invoice-pagination-bar">
+					<div class="col-lg-12 navbar-container">
+						<div class="row">
+							<div class="col-md-6 dataTables_info"></div>
+							<!-- di sini di append buat yang kalimat showing 1 .. 25 dari 123123 baris -->
+							<div class="col-md-6 dataTables_paginate"></div>
+						</div>
+					</div>
+					</nav>
+
+					<div id="refresh-overlay" style="color: white; display: none;">
+						<div class="overlay text-center">
+							<svg class="spinner" viewBox="0 0 66 66"
+								xmlns="http://www.w3.org/2000/svg"> <circle class="path" cx="33"
+								cy="33" fill="none" r="30" stroke-linecap="round"
+								stroke-width="6"></circle> </svg>
+							<div class="refresh-feed"
+								style="margin-top: 50px; font-size: large">Memuat data...</div>
+						</div>
+						<div class="overlay-background"></div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- /.container-fluid-->
+		<!-- /.content-wrapper-->
+		<footer class="sticky-footer">
+		<div class="container">
+			<div class="text-center">
+				<small>Copyright © Your Website 2018</small>
+			</div>
+		</div>
+		</footer>
+		<!-- Scroll to Top Button-->
+		<a class="scroll-to-top rounded" href="#page-top"> <i
+			class="fa fa-angle-up"></i>
+		</a>
+		<!-- Logout Modal-->
+		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+			aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+						<button class="close" type="button" data-dismiss="modal"
+							aria-label="Close">
+							<span aria-hidden="true">×</span>
+						</button>
+					</div>
+					<div class="modal-body">Select "Logout" below if you are ready to
+						end your current session.</div>
+					<div class="modal-footer">
+						<button class="btn btn-secondary" type="button"
+							data-dismiss="modal">Cancel</button>
+						<a class="btn btn-primary" href="login.html">Logout</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Bootstrap core JavaScript-->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<!-- Core plugin JavaScript-->
+		<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+		<!-- Page level plugin JavaScript-->
+		<script src="vendor/chart.js/Chart.min.js"></script>
+		<script src="vendor/datatables/jquery.dataTables.js"></script>
+		<script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+		<!-- Custom scripts for all pages-->
+		<script src="js/sb-admin.min.js"></script>
+		<!-- Custom scripts for this page-->
+		<script src="js/sb-admin-datatables.min.js"></script>
+		<script src="js/sb-admin-charts.min.js"></script>
+	</div>
